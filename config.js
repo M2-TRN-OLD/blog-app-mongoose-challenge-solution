4f3b0f05666b40c59bcf6413f0b1b818a971ebda
Missy Maloney <missymaloney1@gmail.com>
2018-12-29 12:48:37 -0500

    changed testdb to tempblog-db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-db';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/tempBlogPostDb';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/tempblog-db';
 exports.PORT = process.env.PORT || 8080;
